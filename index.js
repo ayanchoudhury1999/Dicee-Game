de03f48c73ab545efb34956ea377bfe283e30283
Ayan Choudhury <76261062+ayanchoudhury1999@users.noreply.github.com>
2021-07-13 10:13:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function rollTheDice() {
+var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
+var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
+document.querySelector(".img1").setAttribute("src", "images/dice" +randomNumber1+ ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" +randomNumber2+ ".png");
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player-1 Wins ";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player-2 Wins🚩";
+    }
+    
+else{
+    document.querySelector("h1").innerHTML = "Draw🤝🤝 ";
+}
+
+}   
+document.querySelector("body").onload = checkRefresh();  
+
+function checkRefresh() {
+    if (!sessionStorage.getItem("rollDice")) {
+      sessionStorage.setItem("rollDice", "extra")
+   
+    } else(
+      rollTheDice()
+    )
+   
+  }
+   
